ebde6a6557ede289953c3db3f658c1ac8b37f59f
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-06-26 10:20:57 -0600

    add init methods for stock

--- service/serviceStock/StockCrud.py
+++ service/serviceStock/StockCrud.py
@@ -16,3 +16,50 @@ class StockCrud(StockServices):
     def close_connection_db(self) -> None:
         self._connection_db.commit()
         self._connection_db.close()
+    '''
+    def create_product(self, product: dict):
+        try:
+            self.init_connection_db()
+            cursor = self._connection_db.cursor()
+            query_select = "SELECT * FROM  WHERE id_usuario = %s"
+            cursor.execute(query_select, (user['id_usuario'],))
+            existing_user = cursor.fetchone()
+            # Si no existe el usuario, lo insertamos en la db, si no, no realiza la insercion
+            if not existing_user:
+                query_insert = """
+                    INSERT INTO usuario (
+                        id_usuario, 
+                        nombre_usuario, 
+                        apellido_paterno, 
+                        apellido_materno, 
+                        correo_usuario, 
+                        password_usuario,
+                        rol_usuario, 
+                        id_area, 
+                        id_equipo
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """
+                cursor.execute(query_insert, (
+                    user['id_usuario'], 
+                    user['nombre_usuario'],
+                    user['apellido_paterno'],
+                    user['apellido_materno'], 
+                    user['correo_usuario'],
+                    self.hash_password(user['password_usuario']),
+                    user['rol_usuario'],
+                    user['id_area'],
+                    user['id_equipo']
+                ))
+                cursor.close()
+                self.close_connection_db()
+                print('Usuario insertado', user)
+                return 'Usuario insertado', 200
+            else:
+                self.close_connection_db()
+                print("El usuario ya existe en la base de datos. No se ha realizado la inserción.")
+                return 'El usuario ya existe en la base de datos. No se ha realizado la inserción.', 400
+        except Exception as e:
+            self.close_connection_db()   
+            print('Error al insertar usuario', e)
+            return 'Error al insertar usuario', 500
+    '''
